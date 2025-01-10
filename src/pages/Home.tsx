3210f5fd3d3fd76d88874fcb2a11b8b0371a5645
Asta1984 <salilmandal908@gmail.com>
2025-01-10 22:46:26 +0530

    10jan25

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,12 @@ import { Link } from 'react-router-dom';
 
 export default function Hero() {
   return (
-    <div className="relative">
-      <div className="grid grid-cols-1 gap-8">
+      <div className="grid grid-cols-1  gap-8">
         <section className="py-16 md:py-20 px-8 md:px-20 mb-16">
           <h1 className="text-2xl sm:text-4xl font-OnlinePrivileges text-wrap max-w-md mb-8 bg-clip-text text-transparent bg-gradient-to-r from-teal-700 to-lime-500 mt-10">
             Launching your ideas has never been easier
           </h1>
-          <p className="text-xl sm:text-2xl font-OnlinePrivileges mb-5 bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-cyan-400">
+          <p className="text-xl sm:text-2xl font-OnlinePrivileges mb-5 bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-cyan-400">
             Build and Launch Your MVP in Record Time. 
           </p>
           <p className='text-xs text-wrap max-w-md font-Type_writer mb-12'>
@@ -21,12 +20,11 @@ export default function Hero() {
               Explore
             </Link>
           </div>
+          <div className="-z-50 md:absolute md:inset-y-0 md:right-0 md:w-1/2">
+            <AnimateSvg />
+          </div>
         </section>
       </div>
-      <div className="relative md:absolute md:right-0 md:w-1/2">
-          <AnimateSvg />
-        </div>
-    </div>
   );
 }
 
