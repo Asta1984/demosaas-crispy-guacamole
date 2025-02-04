e13121e5d2d042eb50cca6e821f7d619032c8f5e
Asta1984 <salilmandal908@gmail.com>
2025-02-04 13:30:02 +0530

    debbug

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,6 @@ import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import AppRoutes from './routes/routes';
 import { AnimatePresence } from 'framer-motion';
-import InfiniteGrid from "@/components/Grid";
-import Spacetimefabric from "@/components/SquareBackground";
-import Mazerunner from "@/components/Warp";
-import Landing from "@/components/Landing";
-import { Footer } from "@/components/ui/footer-section";
 import { ThemeProvider } from './components/theme-provider.tsx'
 
 
