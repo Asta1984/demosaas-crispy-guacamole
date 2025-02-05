0f949c43325e0a3b3b2320d32fabf24ccca2ca50
Asta1984 <salilmandal908@gmail.com>
2025-02-05 20:51:39 +0530

    gradientanimationtoggle

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import "./styles/fonts.css"
-//import './App.css'
+import './App.css'
 import { ThemeProvider } from './components/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
