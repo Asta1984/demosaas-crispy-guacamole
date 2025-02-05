e5b6858b844e963bf548a5b6c6f757133b355034
Asta1984 <salilmandal908@gmail.com>
2025-02-05 20:01:27 +0530

    dark mode oprational

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './App.css'
 import App from './App.tsx'
+import "./styles/fonts.css"
+//import './App.css'
+import { ThemeProvider } from './components/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ThemeProvider>
       <App />
+    </ThemeProvider>
   </StrictMode>,
 )
